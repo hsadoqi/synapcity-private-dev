aa20d2bf27766c161745980bb66f0471a9978dee
Hanaa Sadoqi <hsadoqi@gmail.com>
2026-07-09 19:43:50 -0400

    fix: added root to next config to resolve workspace errors

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
+  turbopack: {
+    root: "/Users/hanaasadoqi/Development/projects/synapcity-private-dev/synapcity-theme"
+  }
 }
 
 export default nextConfig
